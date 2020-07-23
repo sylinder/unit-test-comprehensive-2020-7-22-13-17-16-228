8c087a793a133ac78dee7e175f3f6ee91b6778ab
yuanzhu <yzsylinder@163.com>
2020-07-23 22:47:47 +0800

    [caps] refactor: use HashMap to handle all situation in advance

--- src/main/java/example/GuessNumber.java
+++ src/main/java/example/GuessNumber.java
@@ -1,7 +1,6 @@
 package example;
 
 import java.util.HashMap;
-import java.util.HashSet;
 
 public class GuessNumber {
     public String guess(int[] answer, int[] givenNumber) {
@@ -10,53 +9,18 @@ public class GuessNumber {
             answerItemToIndex.put(answer[index], index);
         }
 
-
         int countCorrectPosition = 0;
-        for (int index = 0; index < answer.length; index++) {
-            if (answer[index] == givenNumber[index]) {
-                countCorrectPosition++;
-            }
-        }
-
-
-
-        if (countCorrectPosition == 4) {
-            return "4A0B";
-        }
-
-        int newCountCorrectPosition = 0;
-        int newWrongPositionCorrectNumber = 0;
+        int wrongPositionCorrectNumber = 0;
         for (int index = 0; index < givenNumber.length; index++) {
-            int tmp = givenNumber[index];
-            if (answerItemToIndex.containsKey(tmp)) {
-                if (answerItemToIndex.get(tmp) == index) {
-                    newCountCorrectPosition++;
+            if (answerItemToIndex.containsKey(givenNumber[index])) {
+                if (answerItemToIndex.get(givenNumber[index]) == index) {
+                    countCorrectPosition++;
                 } else {
-                    newWrongPositionCorrectNumber++;
+                    wrongPositionCorrectNumber++;
                 }
             }
         }
-        return String.format("%dA%dB",newCountCorrectPosition, newWrongPositionCorrectNumber);
+        return String.format("%dA%dB",countCorrectPosition, wrongPositionCorrectNumber);
 
-//        if (countCorrectPosition == 1) {
-//            int countWrongPositionCurrentNumber = 0;
-//            for (int item : givenNumber) {
-//                if (answerItemToIndex.containsKey(item)) {
-//                    countWrongPositionCurrentNumber++;
-//                }
-//            }
-//            return String.format("1A%dB",countWrongPositionCurrentNumber);
-//        }
-
-//        if (countCorrectPosition == 0) {
-//            int countWrongPositionCurrentNumber = 0;
-//            for (int index = 0; index < givenNumber.length; index++) {
-//                if (answerItemToIndex.containsKey(givenNumber[index])) {
-//                    countWrongPositionCurrentNumber++;
-//                }
-//            }
-//            return String.format("0A%dB", countWrongPositionCurrentNumber);
-//        }
-//        return null;
     }
 }
