2b28cc2fac0b7fcfd98e0aedd17695ad33c7d332
yuanzhu <yzsylinder@163.com>
2020-07-23 21:57:46 +0800

    [caps] feature:pass should_return_1A0B_when_guess_given_1567

--- src/main/java/example/GuessNumber.java
+++ src/main/java/example/GuessNumber.java
@@ -2,11 +2,19 @@ package example;
 
 public class GuessNumber {
     public String guess(int[] answer, int[] givenNumber) {
+        int countCorrectPosition = 0;
         for (int index = 0; index < answer.length; index++) {
             if (answer[index] != givenNumber[index]) {
-                return null;
+                break;
             }
+            countCorrectPosition++;
         }
-        return "4A0B";
+        if (countCorrectPosition == 4) {
+            return "4A0B";
+        }
+        if (countCorrectPosition == 1) {
+            return "1A0B";
+        }
+        return null;
     }
 }
