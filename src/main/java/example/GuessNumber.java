72670290801e3ca78c8867540c3e3a6d7fdc6aa0
yuanzhu <yzsylinder@163.com>
2020-07-23 22:08:58 +0800

    [caps] feature:pass should_return_0A2B_when_guess_given_2478

--- src/main/java/example/GuessNumber.java
+++ src/main/java/example/GuessNumber.java
@@ -1,7 +1,13 @@
 package example;
 
+import java.util.HashSet;
+
 public class GuessNumber {
     public String guess(int[] answer, int[] givenNumber) {
+        HashSet<Integer> hashSet = new HashSet<>();
+        for (int item : answer) {
+            hashSet.add(item);
+        }
         int countCorrectPosition = 0;
         for (int index = 0; index < answer.length; index++) {
             if (answer[index] != givenNumber[index]) {
@@ -14,6 +20,16 @@ public class GuessNumber {
         }
         if (countCorrectPosition == 1) {
             return "1A0B";
+        }
+
+        if (countCorrectPosition == 0) {
+            int countWrongPositionCurrentNumber = 0;
+            for (int index = 0; index < givenNumber.length; index++) {
+                if (hashSet.contains(givenNumber[index])) {
+                    countWrongPositionCurrentNumber++;
+                }
+            }
+            return String.format("0A%dB", countWrongPositionCurrentNumber);
         }
         return null;
     }
