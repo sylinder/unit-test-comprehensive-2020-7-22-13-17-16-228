c42ecd48107571728fb154aaa0e020396ac2d7d2
yuanzhu <yzsylinder@163.com>
2020-07-23 22:41:08 +0800

    [caps] feature:pass should_return_1A2B_when_guess_given_0324

--- src/main/java/example/GuessNumber.java
+++ src/main/java/example/GuessNumber.java
@@ -1,36 +1,62 @@
 package example;
 
+import java.util.HashMap;
 import java.util.HashSet;
 
 public class GuessNumber {
     public String guess(int[] answer, int[] givenNumber) {
-        HashSet<Integer> hashSet = new HashSet<>();
-        for (int item : answer) {
-            hashSet.add(item);
+        HashMap<Integer, Integer> answerItemToIndex = new HashMap<>();
+        for (int index = 0; index < answer.length; index++) {
+            answerItemToIndex.put(answer[index], index);
         }
+
+
         int countCorrectPosition = 0;
         for (int index = 0; index < answer.length; index++) {
-            if (answer[index] != givenNumber[index]) {
-                break;
+            if (answer[index] == givenNumber[index]) {
+                countCorrectPosition++;
             }
-            countCorrectPosition++;
         }
+
+
+
         if (countCorrectPosition == 4) {
             return "4A0B";
         }
-        if (countCorrectPosition == 1) {
-            return "1A0B";
-        }
 
-        if (countCorrectPosition == 0) {
-            int countWrongPositionCurrentNumber = 0;
-            for (int index = 0; index < givenNumber.length; index++) {
-                if (hashSet.contains(givenNumber[index])) {
-                    countWrongPositionCurrentNumber++;
+        int newCountCorrectPosition = 0;
+        int newWrongPositionCorrectNumber = 0;
+        for (int index = 0; index < givenNumber.length; index++) {
+            int tmp = givenNumber[index];
+            if (answerItemToIndex.containsKey(tmp)) {
+                if (answerItemToIndex.get(tmp) == index) {
+                    newCountCorrectPosition++;
+                } else {
+                    newWrongPositionCorrectNumber++;
                 }
             }
-            return String.format("0A%dB", countWrongPositionCurrentNumber);
         }
-        return null;
+        return String.format("%dA%dB",newCountCorrectPosition, newWrongPositionCorrectNumber);
+
+//        if (countCorrectPosition == 1) {
+//            int countWrongPositionCurrentNumber = 0;
+//            for (int item : givenNumber) {
+//                if (answerItemToIndex.containsKey(item)) {
+//                    countWrongPositionCurrentNumber++;
+//                }
+//            }
+//            return String.format("1A%dB",countWrongPositionCurrentNumber);
+//        }
+
+//        if (countCorrectPosition == 0) {
+//            int countWrongPositionCurrentNumber = 0;
+//            for (int index = 0; index < givenNumber.length; index++) {
+//                if (answerItemToIndex.containsKey(givenNumber[index])) {
+//                    countWrongPositionCurrentNumber++;
+//                }
+//            }
+//            return String.format("0A%dB", countWrongPositionCurrentNumber);
+//        }
+//        return null;
     }
 }
